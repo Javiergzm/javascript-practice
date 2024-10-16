76724c3e114ce4b402b34b74dcb768fadb8c4cab
Javiergzm <Javier.guzman@stu.vertexacademies.org>
2024-10-16 14:10:09 -0400

    Add files via upload

---
+++ Javier_Guzman_JS_02.js
@@ -0,0 +1,18 @@
+// Example:             // Expected Output
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(3 ** 6);  //expected outcome 729
+console.log(2 * 5);   //10
+console.log(60 / 5.5); //10.909090909090908
+console.log(18 % 4); //2
+console.log(!!false); 	
+
+console.log(2 ** 3);
+console.log(-4 * 1.5); 
+console.log(25 / 8); 
+console.log(18 % 6); 
+console.log(true || true); 
+console.log(false || true); 		
+console.log(true || true); 
+console.log(false || true); 
