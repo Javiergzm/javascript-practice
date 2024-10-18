f0f2805552d34bac1c8caf67bc264445e7c61c00
Javiergzm <Javier.guzman@stu.vertexacademies.org>
2024-10-18 13:35:32 -0400

    Add files via upload

---
+++ Javier_Guzman_JS_03.js
@@ -0,0 +1,18 @@
+console.log(4 > 5);
+console.log(4 == 4 && 10 < 15); 	
+console.log(!true || false); 
+
+let num1 = 5;
+
+let num2 = 6;
+
+console.log(num1 + num2);
+
+let name;  //declared, not assigned
+
+console.log(name);
+
+let Fname = "Javier";
+let Lname = "Guzman";
+
+console.log("Full Name:", Fname +" " +Lname)
