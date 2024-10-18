b712fb193a27cf22354fc30fec6a035c8af6ccd4
Javiergzm <Javier.guzman@stu.vertexacademies.org>
2024-10-18 14:12:11 -0400

    Add files via upload

---
+++ Javier_Guzman_JS_04.js
@@ -0,0 +1,41 @@
+// Assign the variable 'favFruit' with a string name of your favorite fruit.
+
+let favFruit= "Favorite Fruit: Dragonfruit";
+
+console.log(favFruit);
+
+let Price= "Price: 3.00 "
+
+console.log(Price)
+
+let Weight= "Weight: 20"
+
+console.log(Weight)
+
+let Cost= "Total Cost: 60"
+
+console.log(Cost)
+
+
+
+
+
+
+// Declare a variable called 'price' and assign it a number that represents
+// the cost of 1 pound of your favorite fruit. (You can estimate.)
+
+
+
+// Declare a variable called 'pounds' and assign it a number that represents
+// a reasonable number of pounds of fruit to buy.
+
+
+
+// Log this information to the console in an appealing format.
+// Calculate the total cost and log it under the previous information.
+
+// Example:
+//            Favorite Fruit: Apple
+//            Price: $1.25 per pound
+//            Weight: 2.5lbs
+//            Total Cost: $3.125
